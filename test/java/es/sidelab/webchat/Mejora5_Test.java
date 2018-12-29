cd5cbe361f8597eb84def78b73d9cceaeda0b795
Sergio Arroutbi Braojos <sergio.arroutbi.braojos@ericsson.com>
2018-12-29 16:59:50 +0100

    Add improvement 5 test skeleton

---
+++ test/java/es/sidelab/webchat/Mejora5_Test.java
@@ -0,0 +1,31 @@
+package es.sidelab.webchat;
+
+import org.junit.Test;
+
+public class Mejora5_Test {
+	@Test
+	public void GivenAChatManagerWhenChatIsCreatedThenNoErrorOccurs() {
+		
+	}
+
+	@Test
+	public void GivenAChatManagerWhenExistingChatIsRemovedThenNoErrorOccurs() {
+		
+	}
+
+	@Test
+	public void GivenAnExistingChatWhenUserIsRegisteredThenNoErrorOccursAndRestOfUsersAreNotified() {
+		
+	}
+	
+	@Test
+	public void GivenAnExistingChatWhenUserExitsThenNoErrorOccursAndRestOfUsersAreNotified() {
+		
+	}
+	
+	@Test
+	public void GivenAnExistingChatWhenUserSendsMessageThenNoErrorOccursAndRestOfUsersAreNotified() {
+		
+	}
+
+}
